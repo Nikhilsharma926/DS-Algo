173cae17cbfce8c6b759917c519da68d41138356
Nikhilsharam962 <niksharma787683@gmail.com>
2024-10-13 09:59:18 +0530

    while loop syntax

---
+++ Basics/whle.java
@@ -0,0 +1,10 @@
+public class whle {
+    public static void main(String args[]){
+        int counter =0;
+        while(counter<20){
+            System.out.println("hello world");
+            counter++;
+        }
+    }
+    
+}
