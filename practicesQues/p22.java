88b9fac9ceb83231c338c2365c1debfd2417aaf7
Nikhilsharam962 <niksharma787683@gmail.com>
2024-10-14 11:43:18 +0530

    find area of the triangle

---
+++ practicesQues/p22.java
@@ -0,0 +1,16 @@
+package practicesQues;
+import java.util.*;
+
+public class p22 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the base");
+        int base =sc.nextInt();
+        System.out.print("Enter the height");
+        int height =sc.nextInt();
+        double area  = 0.5*base*height;
+        System.out.println(area);
+
+
+    }
+}
