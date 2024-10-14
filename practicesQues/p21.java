99ef2eec6f75c1fc32a17a7ca83f98f8358bcb1a
Nikhilsharam962 <niksharma787683@gmail.com>
2024-10-14 11:26:10 +0530

    find area of the circle

---
+++ practicesQues/p21.java
@@ -0,0 +1,14 @@
+package practicesQues;
+import java.util.*;
+
+public class p21 {
+    public static void main(String args[]){
+       Scanner sc = new Scanner(System.in);
+       System.out.print("enter the circle radius: ");
+       double radius = sc.nextDouble();
+       double area= Math.PI*radius*radius;
+       System.out.println(area);
+
+    }
+    
+}
