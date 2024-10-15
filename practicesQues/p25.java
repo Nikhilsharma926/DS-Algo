e9bb947ad5f2c6f5dd6f0efd885d39bea0e57e2a
Nikhilsharam962 <niksharma787683@gmail.com>
2024-10-15 11:05:43 +0530

    area of parallelogram

---
+++ practicesQues/p25.java
@@ -0,0 +1,15 @@
+package practicesQues;
+import java.util.*;
+
+public class p25 {
+    public static void main (String args []){
+        System.out.println("enter the Base and height :");
+        Scanner sc = new Scanner(System.in);
+        double base = sc.nextDouble();
+        double height = sc.nextDouble();
+        double area = base*height;
+        System.out.print(area);
+
+    }
+    
+}
