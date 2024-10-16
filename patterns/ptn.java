47c9f432a3d83430589c7712759dff9e8f98ae13
Nikhilsharam962 <niksharma787683@gmail.com>
2024-10-16 10:45:18 +0530

    print star  patterns

---
+++ patterns/ptn.java
@@ -0,0 +1,10 @@
+package patterns;
+
+public class ptn {
+    public static void main(String args[]){
+        for(int i =1; i<=4;i++){
+            System.out.println("* * * *");
+        }
+        
+    }
+}
