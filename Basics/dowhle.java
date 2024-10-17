83d003d17dc2822d67ac94b3400351c0e568f4d2
Nikhilsharam962 <niksharma787683@gmail.com>
2024-10-17 22:59:01 +0530

    dowhile loop syntax

---
+++ Basics/dowhle.java
@@ -0,0 +1,12 @@
+
+public class dowhle {
+    public static void main(String args []){
+        int counter = 1;
+        do {
+            System.out.print("hello java");
+            counter++;
+        }
+        while(counter<=10);
+    }
+    
+}
