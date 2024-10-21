7a64cf6931a10c84207469fbca89c18b9ab892c9
Nikhil Sharma <140837201+Nikhilsharma926@users.noreply.github.com>
2024-10-21 14:09:22 +0530

    function overloading

---
+++ fxnction/fxnover.java
@@ -0,0 +1,18 @@
+package fxnction;
+
+public class fxnover {
+    public static int sum (int a , int b){
+        return a+b;
+    }
+    public static int sum (int a , int b, int c){
+        return a+b+c;
+    }
+    // same as for data types ----> like float double etc.
+    public static void main (String args []){
+        System.out.println(sum(4, 4));
+        System.out.print(sum(1, 3, 4));
+    
+
+    }
+    
+}
