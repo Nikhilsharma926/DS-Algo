04c64d74cc50e3b77f48dae739583c28c597286f
Nikhilsharam962 <niksharma787683@gmail.com>
2024-10-22 16:40:14 +0530

    find product of the TWo no. using fxn calling

---
+++ fxnction/product.java
@@ -0,0 +1,15 @@
+package fxnction;
+
+public class product {
+    public static int multiply(int a , int b){
+        int prod =a*b;
+        return prod;
+
+    }
+    public static void main(String args []){
+
+int ml =multiply(6, 2);
+System.out.println(ml);
+    }
+    
+}
