18088b7ff0641f1fe61bc3d67c65c55364b179e0
Kira <140837201+Nikhilsharma926@users.noreply.github.com>
2024-10-24 21:26:07 +0530

    fxn

---
+++ practicesQues/p36.java
@@ -0,0 +1,13 @@
+package practicesQues;
+
+public class p36 {
+    public static int AverSum(int a , int b ,int c){
+        int AS= a+b+c/3;
+        return AS;
+    }
+        public static void main(String args []){
+            System.out.println(AverSum(1, 2, 3));
+
+    }
+    
+}
