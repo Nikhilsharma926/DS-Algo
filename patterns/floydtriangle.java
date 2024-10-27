9df2d3f62226328ecbf71872fb2adbeab5bc3d82
Nikhilsharam962 <niksharma787683@gmail.com>
2024-10-27 14:29:02 +0530

    floyd triangle pattern :~)

---
+++ patterns/floydtriangle.java
@@ -0,0 +1,20 @@
+package patterns;
+
+public class floydtriangle {
+    public static void triangle(int n){
+        int counter =1;
+        for(int i =1 ; i<=n ; i++){
+            for(int j =1 ; j<=i ;j++){
+                System.out.print(counter);
+                counter++;
+
+            }
+            System.out.println();
+               
+        }
+    }
+    public static void main(String[] args) {
+        triangle(5);
+    }
+    
+}
