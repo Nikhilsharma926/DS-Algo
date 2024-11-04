db5e95cefb5088fb9c5af0c6a83dea0b4d2f769f
Nikhilsharam962 <niksharma787683@gmail.com>
2024-11-04 21:29:21 +0530

    solid /=/

---
+++ patterns/solidrhombus.java
@@ -0,0 +1,19 @@
+package patterns;
+public class solidrhombus {
+    public static void solid(int n){
+        for(int i =1; i<= n ; i++){
+            for(int j =1 ; j<=(n-i) ; j++){
+                System.out.print(" ");
+            }
+            for(int j=1 ; j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        solid(5);
+        
+    }
+    
+}
